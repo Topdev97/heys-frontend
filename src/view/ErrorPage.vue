36d4536ef1ec38e6cd56b86f5c18d8ddaed4da16
ScriptSavior <laoroney1113@gmail.com>
2022-09-27 01:15:29 +0100

    lint

--- src/view/ErrorPage.vue
+++ src/view/ErrorPage.vue
@@ -8,7 +8,7 @@ const route = useRoute()
 </script>
 <template>
   <div class="text-gray-100">
-    <div class="p-4 bg-thblue1 header-band pos-relative bg-gradient h-[100vh]">
+    <div class="p-4 h-[100vh] bg-thblue1 header-band pos-relative bg-gradient">
       <div class="fixed top-0 left-0 z-50 w-full h-1 bg-thblue2"></div>
       <div class="header-nav">
         <div class="flex justify-between items-center">
@@ -21,7 +21,7 @@ const route = useRoute()
           </router-link>
         </div>
       </div>
-      <div class="header-content flex justify-center items-center flex-col h-full">
+      <div class="flex flex-col justify-center items-center h-full header-content">
         <h1>Error</h1>
         <h4 v-if="route.query.type === 'not-connected'">Install and connect to MetaMask</h4>
         <h4 v-if="route.query.type === 'no-gathering-token-balance'">
